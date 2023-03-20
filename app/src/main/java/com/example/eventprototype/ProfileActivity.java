ba2ad6aa96ad410b5d29d261932ce9d120da3555
kenfun23 <62788319+kenfun23@users.noreply.github.com>
2023-03-20 21:21:25 +1100

    updat eprofiel

--- app/src/main/java/com/example/eventprototype/ProfileActivity.java
+++ app/src/main/java/com/example/eventprototype/ProfileActivity.java
@@ -1,4 +1,38 @@
 package com.example.eventprototype;
 
-public class ProfileActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private Button btnProfileUpdate;
+    private TextView menuTitle;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_page1);
+
+        //set up the toolbar
+        menuTitle = findViewById(R.id.toolbar_heading_only);
+        menuTitle.setText("");
+
+        btnProfileUpdate = findViewById(R.id.profile_update_button);
+
+
+        btnProfileUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v){
+                startActivity(new Intent(ProfileActivity.this, MainActivity.class));
+            }
+        });
+
+    }
+
+
 }
