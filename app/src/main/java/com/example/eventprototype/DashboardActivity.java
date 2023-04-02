a4e6f9a15ca5e80e57b83a359f63672afec0b3cd
unswERM <z5260197@ad.unsw.edu.au>
2023-04-02 13:35:31 +1000

    dashboard

--- app/src/main/java/com/example/eventprototype/DashboardActivity.java
+++ app/src/main/java/com/example/eventprototype/DashboardActivity.java
@@ -33,10 +33,12 @@ public class DashboardActivity extends AppCompatActivity {
 
         //set up dashboard
         dashboardView = findViewById(R.id.dashboardView);
-        String dashboardUrl = "https://app.powerbi.com/";
-        String iframeSrc = "https://app.powerbi.com/view?r=eyJrIjoiODFlMWJlNmYtNjFjYi00ZmY5LTk2ZmEtNWM3MmQwNTFlNWU3IiwidCI6IjliN2JmYmI2LTllN2EtNDFlYS05NDIwLThhY2VkNDM3ZDM0YiJ9";
-        String htmlData = "<iframe title=\"Report Section\" width=\"380\" height=\"500\" src=\'" + iframeSrc + "'' frameborder=\"0\" allowFullScreen=\"true\"></iframe>";
-        dashboardView.loadDataWithBaseURL(dashboardUrl, htmlData, "text/html", "UTF-8", null);
+        //String dashboardUrl = "https://public.tableau.com/shared/MHTWW4D6W?:display_count=n&:origin=viz_share_link&:embed=y";
+        //String htmlData = "<div class='tableauPlaceholder' id='viz1680405248722' style='position: relative'><noscript><a href='https:&#47;&#47;www.mckinsey.com&#47;global-themes&#47;artificial-intelligence&#47;notes-from-the-ai-frontier-applications-and-value-of-deep-learning'><img alt='Estimated impact of artificial intelligence and other analytics by industry, function and business problem. ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;ST&#47;STDCFJM79&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='path' value='shared&#47;STDCFJM79' /> <param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;ST&#47;STDCFJM79&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='filter' value='showVizhome=no' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1680405248722');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='640px';vizElement.style.height='1227px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>";
+        //dashboardView.loadDataWithBaseURL(dashboardUrl, htmlData, "text/html", "UTF-8", null);
+
+        dashboardView.getSettings().setJavaScriptEnabled(true);
+        dashboardView.loadUrl("https://public.tableau.com/shared/MHTWW4D6W?:showVizHome=no");
 
         //set up the toolbar
         menuTitle = findViewById(R.id.toolbar_heading_logout);
