b817d2acc09bb3382026016de9cedfa995f359f1
siyuhan2805 <siyu.han2805@gmail.com>
2023-04-15 04:29:16 +1000

    Google maps scroll functionality

--- app/src/main/java/com/example/eventprototype/DashboardActivity.java
+++ app/src/main/java/com/example/eventprototype/DashboardActivity.java
@@ -2,16 +2,20 @@ package com.example.eventprototype;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.ScrollView;
 
 import com.example.eventprototype.Model.UserModel;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
@@ -21,8 +25,8 @@ public class DashboardActivity extends AppCompatActivity {
 
     private ArrayList<UserModel> currentUser;
 
-    private ScrollView dashScroll;
-    private ImageView transImage;
+    private NestedScrollView dashScroll;
+    private ImageView transparent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +35,43 @@ public class DashboardActivity extends AppCompatActivity {
         //get the intent passed from the LoginActivity
         currentUser = (ArrayList<UserModel>) getIntent().getSerializableExtra("currentUser");
         System.out.println(currentUser.get(0).getUsername());
-        //ScrollView dashScroll = findViewById(R.id.dashScroll);
-        //transImage = findViewById(R.id.imageTrans);
+
+        dashScroll = findViewById(R.id.dashScroll);
+        transparent = findViewById(R.id.imageTrans);
+
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
         //set the icon as selected
         bottomNavigationView.setSelectedItemId(R.id.dashboard);
-        Fragment fragment = new MapFragment();
+        Fragment fragment = new SupportMapFragment();
         getSupportFragmentManager().beginTransaction().replace(R.id.eventMapFrame, fragment).commit();
+
+        //workaround to make sure you can scroll on the Maps
+
+        transparent.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = event.getAction();
+                switch (action) {
+                    case MotionEvent.ACTION_DOWN:
+                        // Disallow ScrollView to intercept touch events.
+                        dashScroll.requestDisallowInterceptTouchEvent(true);
+                        // Disable touch on transparent view
+                        return false;
+
+                    case MotionEvent.ACTION_UP:
+                        // Allow ScrollView to intercept touch events.
+                        dashScroll.requestDisallowInterceptTouchEvent(false);
+                        return true;
+
+                    case MotionEvent.ACTION_MOVE:
+                        dashScroll.requestDisallowInterceptTouchEvent(true);
+                        return false;
+
+                    default:
+                        return true;
+                }
+            }
+        });
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
--- app/src/main/java/com/example/eventprototype/MapFragment.java
+++ app/src/main/java/com/example/eventprototype/MapFragment.java
@@ -5,11 +5,13 @@ import android.location.Geocoder;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -27,38 +29,32 @@ public class MapFragment extends Fragment {
     private GoogleMap map;
     private List<Address> listGeoCoder;
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_map,container,false);
         SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.eventMap);
+        System.out.println("Fragment Loaded");
         try {
             listGeoCoder = new Geocoder(getContext()).getFromLocationName("Sydney", 1);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
         supportMapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(@NonNull GoogleMap googleMap) {
-                googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-                    @Override
-                    public void onMapClick(@NonNull LatLng latLng) {
-                        map = googleMap;
-                        LatLng sydney = new LatLng(listGeoCoder.get(0).getLatitude(),listGeoCoder.get(0).getLongitude());
-                        map.addMarker(new MarkerOptions().position(sydney).title("Sydeny Event"));
-                        map.clear();
-                        map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 20));
-                        map.getUiSettings().setZoomControlsEnabled(true);
-
-                    }
-                });
-
-
+                map = googleMap;
+                LatLng sydney = new LatLng(listGeoCoder.get(0).getLatitude(),listGeoCoder.get(0).getLongitude());
+                System.out.println(sydney);
+                map.addMarker(new MarkerOptions().position(sydney).title("Sydeny Event"));
+                map.getUiSettings().setZoomControlsEnabled(true);
 
             }
         });
         return view;
     }
+
+
 }
